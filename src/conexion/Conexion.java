f59c185db507d6a3589d207b729e45830698e6bd
StyleJava <149973059+StyleJava@users.noreply.github.com>
2023-11-05 19:46:47 -0500

    Update Conexion.java

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -9,12 +9,10 @@ import javax.swing.JOptionPane;
 public class Conexion {
   
    public static Connection con;
-    // Declaramos los datos de conexion a la bd
    
     private static final String user="root";
     private static final String pass="13062002";
     private static final String url="jdbc:mysql://localhost:3305/bd_login";
-    // Funcion que va conectarse a mi bd de mysql
     public static Connection conectar(){
       con = null;
       try{
